f84cf9d25e25ab7c40c09ce2080e586cd0a7520a
Vladimir Kozlovskyi <vlad.kozlovskyi@gmail.com>
2016-12-14 23:11:58 +0200

    iOS push cert added

--- index.js
+++ index.js
@@ -19,7 +19,16 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+  push: {
+		ios: {
+			pfx: 'certs/cert.p12',
+			cert: '',
+			bundleId: '',
+			key: '',
+			production: true
+		}
+	}
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
